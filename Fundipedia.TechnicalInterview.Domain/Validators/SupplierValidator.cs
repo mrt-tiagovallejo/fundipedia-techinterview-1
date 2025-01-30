5b5b7e789fd9cb86c42436ec271b277de9f09c1e
Tiago Vallejo <tiago@tvallejo.net>
2025-01-30 17:48:52 +0000

    refactor: improved SupplierValidator readability

--- Fundipedia.TechnicalInterview.Domain/Validators/SupplierValidator.cs
+++ Fundipedia.TechnicalInterview.Domain/Validators/SupplierValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Fundipedia.TechnicalInterview.Model.Supplier;
 using System;
+using System.Collections.Generic;
 
 namespace Fundipedia.TechnicalInterview.Domain.Validators
 {
@@ -18,33 +19,58 @@ namespace Fundipedia.TechnicalInterview.Domain.Validators
         {
             var result = new ValidationResult();
 
-            // Validate activation date
-            if (supplier.ActivationDate <= DateTime.Today)
+            ValidateActivationDate(supplier.ActivationDate, result);
+            ValidateEmails(supplier.Emails, result);
+            ValidatePhones(supplier.Phones, result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Validates activation date
+        /// </summary>
+        /// <param name="activationDate"></param>
+        /// <param name="result"></param>
+        private void ValidateActivationDate(DateTime activationDate, ValidationResult result)
+        {
+            if (activationDate <= DateTime.Today)
             {
                 result.AddError("Activation date must be tomorrow or later.");
             }
+        }
 
-            // Validate email format if existent
-            // (assuming email is not mandatory)
-            foreach (var email in supplier.Emails)
+        /// <summary>
+        /// Validates email format if existent
+        /// (assuming email is not mandatory)
+        /// </summary>
+        /// <param name="emails"></param>
+        /// <param name="result"></param>
+        private void ValidateEmails(ICollection<Email> emails, ValidationResult result)
+        {
+            foreach (var email in emails)
             {
                 if (!_emailValidator.IsValid(email.EmailAddress))
                 {
                     result.AddError($"Invalid email format: {email.EmailAddress}");
                 }
             }
+        }
 
-            // Validate phone number if existent
-            // (assuming phone number is not mandatory)
-            foreach (var phone in supplier.Phones)
+        /// <summary>
+        /// Validates phone number if existent
+        /// (assuming phone number is not mandatory)
+        /// </summary>
+        /// <param name="phones"></param>
+        /// <param name="result"></param>
+        private void ValidatePhones(ICollection<Phone> phones, ValidationResult result)
+        {
+            foreach (var phone in phones)
             {
                 if (!_phoneValidator.IsValid(phone.PhoneNumber))
                 {
                     result.AddError($"Invalid phone number: {phone.PhoneNumber}");
                 }
             }
-
-            return result;
         }
     }
 }
