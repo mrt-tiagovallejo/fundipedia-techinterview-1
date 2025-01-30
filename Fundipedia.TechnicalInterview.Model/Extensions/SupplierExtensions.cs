48e27c03baf3574ab317ddf8b0157d7c79a226a4
Tiago Vallejo <tiago@tvallejo.net>
2025-01-30 18:00:57 +0000

    fix: SupplierExtensions logic to determine a supplier is active

--- Fundipedia.TechnicalInterview.Model/Extensions/SupplierExtensions.cs
+++ Fundipedia.TechnicalInterview.Model/Extensions/SupplierExtensions.cs
@@ -1,9 +1,11 @@
-﻿namespace Fundipedia.TechnicalInterview.Model.Extensions;
+﻿using System;
+
+namespace Fundipedia.TechnicalInterview.Model.Extensions;
 
 public static class SupplierExtensions
 {
     public static bool IsActive(this Supplier.Supplier supplier)
     {
-        return supplier.ActivationDate != null;
+        return supplier.ActivationDate <= DateTime.Today;
     }
 }
